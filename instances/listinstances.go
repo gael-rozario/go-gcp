4ca221f8fe88b53dbfda856d9ceb5af84da1a892
Gael Rozario <gaelrozario@gmail.com>
2019-11-06 08:18:50 +0530

    adding go routines

--- instances/listinstances.go
+++ instances/listinstances.go
@@ -4,29 +4,45 @@ import (
 	"context"
 	"log"
 	compute "google.golang.org/api/compute/v1"
+	"sync"
 )
 
+var wg sync.WaitGroup
 //GetAllInstances list all instances in the project
 func GetAllInstances(project string) ([]*compute.Instance, error){
 	instanceList := make([]*compute.Instance,0)
-	ctx := context.Background()
-	computeclient, err := compute.NewService(ctx)
+	instanceChannel := make(chan *compute.Instance, 100)
 	zones := GetZones(project)
-	if err != nil {
-		return nil, err
-	}
 	for _, zone := range zones {
-		response, err := computeclient.Instances.List(project, zone).Do()
-		if err != nil {
-			return nil, err
-		}
-		for _,instance := range response.Items{
-			instanceList= append(instanceList,instance)
-		}
+		wg.Add(1)
+		go GetInstanceOverRegion(instanceChannel,project,zone)
 	}
+	wg.Wait()
+	close(instanceChannel)
+	for instance:= range instanceChannel{
+		instanceList = append(instanceList,instance)	
+	}
+
 	return instanceList, nil
 }
 
+//GetInstanceOverRegion retrieves instances in one region
+func GetInstanceOverRegion(instanceChannel chan *compute.Instance,project string, zone string)  {
+	defer wg.Done()
+	ctx := context.Background()
+	computeclient, err := compute.NewService(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	response, err := computeclient.Instances.List(project, zone).Do()
+	if err!= nil{
+		log.Fatal(err)
+	}
+	for _,instance := range response.Items{
+		instanceChannel <-instance
+	}
+	
+}
 //GetZones lists zones available in a project
 func GetZones(p string) []string {
 	ctx := context.Background()
